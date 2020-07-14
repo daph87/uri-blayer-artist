a80398d7589dbadfec6a09ad67047d274e266714
Daphne Levy <87daphnelevy@gmail.com>
2020-07-14 15:28:53 +0300

    modal field + 2019-2020 details

--- src/app/components/field/field.component.ts
+++ src/app/components/field/field.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild, ElementRef } from '@angular/core';
 import { Image } from 'src/models/image';
+import { openModal, closeModal, previousImage, nextImage, onKeyUp } from 'src/app/global';
 
 @Component({
   selector: 'app-field',
@@ -7,14 +8,26 @@ import { Image } from 'src/models/image';
   styleUrls: ['./field.component.scss']
 })
 export class FieldComponent implements OnInit {
-  public images: Image[];
+  public paintings:[];
+  public index: number;
+  public maxLength: number;
+  public painting: any = {};
+
+  @ViewChild('artworkModal', { static: false }) artworkModal: ElementRef;
+  // modal states
+
+  public openModal = openModal;
+  public closeModal = closeModal;
+  public previousImage = previousImage;
+  public nextImage = nextImage;
+  public onKeyUp = onKeyUp;
 
   constructor() { }
 
   ngOnInit(): void {
     // get the images from the json file
     fetch('assets/json/field.json').then(data => data.json())
-    .then(images => this.images = images);
+    .then(images => this.paintings = images);
 
   }
 
